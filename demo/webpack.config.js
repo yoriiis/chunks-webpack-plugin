9824ad002d2cc96b3954924ae289668bad94f5f1
Yoriiis <joris.daniel@gmail.com>
2019-12-05 23:26:49 +0100

    Update to 3.3.0 (see the changelog for details)

---
+++ demo/webpack.config.js
@@ -0,0 +1,105 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserJSPlugin = require('terser-webpack-plugin')
+const ManifestPlugin = require('webpack-manifest-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const ChunksWebpackPlugin = require('../dist/index')
+
+module.exports = (env, argv) => {
+	const isProduction = argv.mode === 'production'
+
+	return {
+		watch: !isProduction,
+		entry: {
+			'app-a': './src/js/app-a.js',
+			'app-b': './src/js/app-b.js'
+		},
+		watchOptions: {
+			ignored: /node_modules/
+		},
+		devtool: 'none',
+		output: {
+			path: path.resolve(__dirname, './dist'),
+			publicPath: '/dist/',
+			filename: 'js/[name].js'
+		},
+		module: {
+			rules: [{
+				test: /\.js$/,
+				use: [{
+					loader: 'babel-loader'
+				}]
+			}, {
+				test: /\.css$/,
+				use: [
+					MiniCssExtractPlugin.loader, {
+						loader: 'css-loader'
+					}
+				]
+			}]
+		},
+		plugins: [
+			new MiniCssExtractPlugin({
+				filename: `css/[name].css`,
+				chunkFilename: `css/[name].css`
+			}),
+			new ChunksWebpackPlugin({
+				outputPath: path.resolve(__dirname, 'dist/templates'),
+				fileExtension: '.html',
+				generateChunksManifest: true,
+				generateChunksFiles: true,
+				customFormatTags: (chunksSorted, chunkGroup) => {
+					// Generate all HTML style tags with CDN prefix
+					const styles = chunksSorted['styles'].map(chunkCss =>
+						`<link rel="stylesheet" href="https://cdn.domain.com${chunkCss}" />`
+					).join('')
+
+					// Generate all HTML style tags with CDN prefix and defer attribute
+					const scripts = chunksSorted['scripts'].map(chunkJs =>
+						`<script defer src="https://cdn.domain.com${chunkJs}"></script>`
+					).join('')
+
+					return { styles, scripts }
+				}
+			}),
+			new ManifestPlugin({
+				writeToFileEmit: true,
+				fileName: 'manifest.json'
+			})
+		],
+		stats: {
+			assets: true,
+			colors: true,
+			hash: false,
+			timings: true,
+			chunks: false,
+			chunkModules: false,
+			modules: false,
+			children: false,
+			entrypoints: false,
+			excludeAssets: /.map$/,
+			assetsSort: '!size'
+		},
+		optimization: {
+			minimizer: [
+				new TerserJSPlugin({
+					extractComments: false
+				}),
+				new OptimizeCSSAssetsPlugin({})
+			],
+			namedChunks: false,
+			namedModules: false,
+			removeAvailableModules: true,
+			removeEmptyChunks: true,
+			mergeDuplicateChunks: true,
+			occurrenceOrder: true,
+			providedExports: false,
+			mangleWasmImports: true,
+			splitChunks: {
+				chunks: 'all',
+				minSize: 0,
+				name: true
+			}
+		}
+	}
+}
