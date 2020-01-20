f8cd855fafd5668420d32d0b72a60f12d931491a
Joris Daniel <joris.daniel@gmail.com>
2020-01-20 18:04:03 +0100

    Update Travis CI with node versions (#38)
    Update to 3.4.5 (see the changelog for details)

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@
 /**
  * @license MIT
  * @name ChunksWebpackPlugin
- * @version 3.4.4
+ * @version 3.4.5
  * @author: Yoriiis aka Joris DANIEL <joris.daniel@gmail.com>
  * @description: Easily create HTML files with all chunks by entrypoint (based on Webpack chunkGroups)
  * {@link https://github.com/yoriiis/chunks-webpack-plugins}
